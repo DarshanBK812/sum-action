c2e299acac361691965e1e725b4c2628a17c39e8
Darshan <darshanbk812@gmail.com>
2025-07-28 00:39:11 +0530

    Update SumController.java
    changing from diff to sum

--- src/main/java/com/darshanbk812/sum_action/SumController.java
+++ src/main/java/com/darshanbk812/sum_action/SumController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SumController {
 
-	@GetMapping("/diff/{n1}/{n2}")
+	@GetMapping("/sum/{n1}/{n2}")
 	public String diff(@PathVariable int n1, @PathVariable int n2) {
-		return "Sum is :"+(n1 - n2);
+		return "Sum is :"+(n1 + n2);
 	}
 
 }
